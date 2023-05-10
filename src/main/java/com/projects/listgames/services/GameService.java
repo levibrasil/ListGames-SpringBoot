c146128b53891cec092aa4dfe9e10401bf9d8a77
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-09 23:13:52 -0300

    Game, repository, service, controller

---
+++ src/main/java/com/projects/listgames/services/GameService.java
@@ -0,0 +1,23 @@
+package com.projects.listgames.services;
+
+import com.projects.listgames.dto.GameMinDTO;
+import com.projects.listgames.entities.Game;
+import com.projects.listgames.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+
+        return dto;
+    }
+}
