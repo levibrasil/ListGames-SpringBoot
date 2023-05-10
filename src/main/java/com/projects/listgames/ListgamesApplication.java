fe04f7109b55d59fd2c15dd42c77ff042f93e0e6
Levi Brasil <levi.mbrasil98dev@gmail.com>
2023-05-09 21:52:49 -0300

    Project created

---
+++ src/main/java/com/projects/listgames/ListgamesApplication.java
@@ -0,0 +1,13 @@
+package com.projects.listgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListgamesApplication.class, args);
+	}
+
+}
